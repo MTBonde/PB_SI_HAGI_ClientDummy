fd79816a75452726fe95fe6efd64c98157e473c5
MTBonde <thorbonde@gmail.com>
2025-10-24 09:02:38 +0200

    Updated DummyClient to RelayService

--- DummyClient/Program.cs
+++ DummyClient/Program.cs
@@ -2,12 +2,16 @@
 using System.Text.Json;
 
 using var httpClient = new HttpClient();
-var baseAuthUrl = "http://localhost:5000";
+var baseAuthUrl = Environment.GetEnvironmentVariable("AUTH_URL") ?? "http://localhost:5000";
 
 // nice intro
 Console.WriteLine($"DummyClient v{DummyClient.ServiceVersion.Current} starting...");
 Console.WriteLine("========================================");
 Console.WriteLine();
+
+// quick nap
+Console.WriteLine("Waiting for services to start...");
+await Task.Delay(5000); // 5 seconds
 
 // Login to AuthService
 try
