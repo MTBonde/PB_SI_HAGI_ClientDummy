463735ca5e6da5ac2598f92d72feaaf805855b9c
MTBonde <thorbonde@gmail.com>
2025-10-27 18:19:25 +0100

    bump service versions (`registryservice` to 0.2.3, `sessionservice` to 0.2.0, `chatservice` to 0.2.0, `relayservice` to 0.3.0); update `DummyClient` version to 0.1.5.

--- DummyClient/ServiceVersion.cs
+++ DummyClient/ServiceVersion.cs
@@ -2,7 +2,7 @@ namespace DummyClient;
 
 public static class ServiceVersion
 {
-    public const string Current = "0.1.3";
+    public const string Current = "0.1.5";
 }
 
 // initial 0.1.0
@@ -10,3 +10,4 @@ public static class ServiceVersion
 // testing registry service 0.1.2
 // testing relayv0.2 websocket 0.1.3
 // fixing relay to v0.2.2 so it matches auth 0.1.4
+// relay v0.3, chat v0.2, session v0.2 registry v0.2.3 = 0.1.5
